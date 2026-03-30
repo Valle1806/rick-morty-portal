fd262a59ccb016993aea1b11a39e8be2dec52e27
Valle1806 <cristianvallecilla18@gmail.com>
2026-03-30 01:28:08 -0500

    chore: configuración inicial del proyecto Angular con SSR

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'rick-morty-app' title`, () => {
+  it(`should have the 'rick-morty-portal' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('rick-morty-app');
+    expect(app.title).toEqual('rick-morty-portal');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, rick-morty-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, rick-morty-portal');
   });
 });
